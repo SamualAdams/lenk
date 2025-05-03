62ad363f16f8da61fd4efbdf8ec8df7964fe6394
Jon E <jon.emas@icloud.com>
2025-05-03 09:11:02 -0500

    refactor frontend

---
+++ frontend/src/components/Navigation.js
@@ -0,0 +1,15 @@
+// src/components/Navigation.js
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+function Navigation() {
+  return (
+    <nav className="bg-gray-800 text-white p-4">
+      <div className="container mx-auto flex justify-between items-center">
+        <Link to="/" className="text-xl font-bold">Cognition Reader</Link>
+      </div>
+    </nav>
+  );
+}
+
+export default Navigation;
